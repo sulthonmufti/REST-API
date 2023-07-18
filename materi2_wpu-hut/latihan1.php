fcee92deb43fae2da5690c8a1c6cb680519ea190
sulthonmufti <msulthonmufti14@gmail.com>
2023-07-18 16:47:00 +0700

    commit-3m

--- materi2_wpu-hut/latihan1.php
+++ materi2_wpu-hut/latihan1.php
@@ -13,7 +13,9 @@
   <body>
 
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">
+        <img src="img/logo.png" width="120">
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
